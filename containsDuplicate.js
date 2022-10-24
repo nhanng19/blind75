834f193b4975fa7886e568c82abbf42a52e08b43
nhan <nhanng19@gmail.com>
2022-10-24 02:04:52 -0700

    first run

---
+++ containsDuplicate.js
@@ -0,0 +1,10 @@
+var containsDuplicates = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array.indexOf(array[i]) !== array.lastIndexOf(array[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+    
+containsDuplicates([1,19,4,5,16,6,9])
