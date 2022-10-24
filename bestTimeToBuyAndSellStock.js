2f2dbb3357ede2f4eaf2ad17d0eeb0655aefef3d
nhan <nhanng19@gmail.com>
2022-10-24 01:44:16 -0700

    first run

---
+++ bestTimeToBuyAndSellStock.js
@@ -0,0 +1,16 @@
+var maxProfit = (prices) => {
+    let profit = 0;
+    let buyStock = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        if (buyStock > prices[i]) {
+            buyStock = prices[i]
+        }
+        let currentProfit = prices[i] - buyStock;
+        if (currentProfit > profit) {
+            profit = currentProfit
+        }
+    }
+    console.log(profit);
+}
+
+maxProfit([7, 1, 5, 3, 6, 4]);
