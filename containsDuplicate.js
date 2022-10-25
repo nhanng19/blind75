58fef8af9cc4ea12a9dc936dec619d8075a68754
nhan <nhanng19@gmail.com>
2022-10-25 05:40:15 -0700

    rerun

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,10 +1,11 @@
-var containsDuplicates = (nums) => {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
-            return true;
-        }
+var containsDuplicates = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array.indexOf(array[i]) !== array.lastIndexOf(array[i])) {
+      return true;
     }
-    return false;
-}
-    
-containsDuplicates([1,19,4,5,16,6,9])
+  }
+  return false;
+};
+
+containsDuplicates([1, 19, 4, 5, 16, 6, 9]);
+
