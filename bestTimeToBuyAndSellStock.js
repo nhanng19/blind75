b2244b6e3390926dd1f4980cbb2d2a79bca9f5ec
nhan <nhanng19@gmail.com>
2022-10-31 14:37:35 -0700

    rerun

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -1,20 +1,18 @@
-const maxProfit = (prices) => {
-    let profit = 0;
-    let buyStock = prices[0];
+const stock = (prices) => {
+  let profit = 0;
+  let buyStock = prices[0];
 
-    for (let i = 1; i < prices.length; i++) {
-        if (buyStock > prices[i]) {
-            buyStock = prices[i]
-        }
-
-        let currentProfit = prices[i] - buyStock;
-        if (currentProfit > profit) {
-            profit = currentProfit
-        }
+  for (let i = 1; i < prices.length; i++) {
+    if (buyStock > prices[i]) {
+      buyStock = prices[i];
     }
-    return profit;
-}
+    let currentProfit = prices[i] - buyStock;
+    if (currentProfit > profit) {
+      profit = currentProfit;
+    }
+  }
+  return profit;
+};
 
-maxProfit([7, 1, 5, 3, 6, 4]);
-
+stock([7, 1, 5, 3, 6, 4]);
 
