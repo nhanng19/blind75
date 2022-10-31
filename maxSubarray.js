9d54833a453830471cfeec3584330f604030667d
nhan <nhanng19@gmail.com>
2022-10-31 15:33:26 -0700

    rerun

--- maxSubarray.js
+++ maxSubarray.js
@@ -1,15 +1,16 @@
 const maxSubArray = (nums) => {
-    maxSum = nums[0];
-    currentSum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (currentSum < 0) {
-            currentSum = 0
-        }
-        currentSum += nums[i];
-        maxSum = Math.max(currentSum, maxSum);
+  let maxSum = nums[0];
+  let currentSum = 0;
+
+  for (i = 0; i < nums.length; i++) {
+    if (currentSum < 0) {
+      currentSum = 0;
     }
-    console.log(maxSum)
-}
+    currentSum += nums[i];
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  console.log(maxSum);
+};
 
 maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
 
