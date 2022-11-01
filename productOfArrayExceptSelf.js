f64a3854c335701eb3667e54eae5e3a072ee1ecd
nhan <nhanng19@gmail.com>
2022-10-31 23:40:34 -0700

    rerurn

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -1,24 +1,6 @@
 product([1, 2, 3, 4]);
 
-const product = nums => {
-  let results = [];
-  let left = 1;
-  let right = 1;
-
-  for (let i = 0; i < nums.length; i++) {
-    results[i] = left;
-    left = left * nums[i];
-  }
-
-  for (let i = nums.length - 1; i >= 0; i--) {
-    results[i] = right * results[i];
-    right = right * nums[i];
-  }
-
-  console.log(results)
-}
-
-const productOfArray = (nums) => {
+const product = (nums) => {
   let results = [];
   let left = 1;
   let right = 1;
@@ -34,4 +16,4 @@ const productOfArray = (nums) => {
   }
 
   return results;
-}
+};
