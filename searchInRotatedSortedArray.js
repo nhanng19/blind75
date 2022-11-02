95e063206c3c6db9c8bfe160f53d382513691f30
nhan <nhanng19@gmail.com>
2022-11-02 15:09:32 -0700

    rerun

--- searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray.js
@@ -1,26 +1,26 @@
+search([1, 3, 5, 6, 7, 8], 6);
+
 const search = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+    let mid = Math.floor((right + left) / 2);
     if (nums[mid] === target) {
-      console.log(mid);
+      return mid
     }
     if (nums[left] <= nums[mid]) {
       if (nums[left] <= target && target <= nums[mid]) {
-        right = mid - 1;
+        right = mid - 1
       } else {
-        left = mid + 1;
+        left = mid + 1
       }
     } else {
       if (nums[mid] <= target && target <= nums[right]) {
         left = mid + 1;
       } else {
-        right = mid - 1;
+        right = mid - 1
       }
     }
   }
-  console.log(-1);
-};
-
-search([1, 3, 5, 6, 7, 8], 6);
+  return -1
+}
