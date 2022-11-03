51a7deb1518a4e24668bdc91627b53e86d5430fb
nhan <nhanng19@gmail.com>
2022-11-02 22:49:09 -0700

    rerun

--- minimumInRotatedSortedArray.js
+++ minimumInRotatedSortedArray.js
@@ -1,16 +1,16 @@
-minimum([10, 8, 13, 12, 5, 9])
-
-
 const minimum = nums => {
     let left = 0;
-    let right = nums.length - 1
+    let right = nums.length - 1;
+
     while (left < right) {
         const mid = left + Math.floor((right - left) / 2)
         if (nums[right] >= nums[mid]) {
-            right = mid
+            right = mid;
         } else {
             left = mid + 1;
         }
     }
-    return nums[left];
+    console.log(nums[left])
 }
+
+minimum([10, 8, 13, 12, 5, 9])
