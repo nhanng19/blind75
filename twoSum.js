8dde31d5e8e085953c9fc1d2ba4a57f4495a71eb
nhan <nhanng19@gmail.com>
2022-11-02 22:48:50 -0700

    rerun

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,7 @@
 twoSum([2, 7, 11, 15], 9);
 
-
 const twoSum = (nums, target) => {
-  let results = [];
+  let results = []
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -11,5 +10,5 @@ const twoSum = (nums, target) => {
       }
     }
   }
-  console.log(results)
+  return results
 }
