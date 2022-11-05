6292e7cd077dedf853e3ef42feaa4281d608c8bc
nhan <nhanng19@gmail.com>
2022-11-05 01:50:16 -0700

    rerun

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,3 +1,5 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
 duplicates([1, 19, 4, 5, 16, 6, 9]);
 
 const duplicates = array => {
