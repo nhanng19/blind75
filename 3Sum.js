ea8d09a80aec0506fbb9894905ba1d126915f1c5
nhan <nhanng19@gmail.com>
2022-11-05 01:50:05 -0700

    rerun

--- 3Sum.js
+++ 3Sum.js
@@ -1,28 +1,32 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+//
+// Notice that the solution set must not contain duplicate triplets.
+
 sumThree([-1, 0, 1, 2, -1, -4]);
 
-const sumThree = nums => {
-    let results = [];
-    nums.sort((a, b) => a - b)
+const sumThree = (nums) => {
+  let results = [];
+  nums.sort((a, b) => a - b);
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] !== nums[i - 1]) {
-            let left = i + 1;
-            let right = nums.length - 1;
-            while (left < right) {
-                let currentSum = nums[i] + nums[left] + nums[right];
-                if (currentSum === 0) {
-                    results.push([nums[i], nums[left], nums[right]]);
-                    while (nums[left] === nums[left + 1]) left++;
-                    while (nums[right] === nums[right - 1]) right--;
-                    left++;
-                    right--
-                } else if (currentSum < 0) {
-                    left++;
-                } else if (currentSum > 0) {
-                    right--;
-                }
-            }
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      let left = i + 1;
+      let right = nums.length - 1;
+      while (left < right) {
+        let currentSum = nums[i] + nums[left] + nums[right];
+        if (currentSum === 0) {
+          results.push([nums[i], nums[left], nums[right]]);
+          while (nums[left] === nums[left + 1]) left++;
+          while (nums[right] === nums[right - 1]) right--;
+          left++;
+          right--;
+        } else if (currentSum < 0) {
+          left++;
+        } else if (currentSum > 0) {
+          right--;
         }
+      }
     }
-    return results;
-}
+  }
+  return results;
+};
