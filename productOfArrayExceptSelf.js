75537baf3c0cdb935a467bbe53b7368424cee179
nhan <nhanng19@gmail.com>
2022-11-05 01:50:48 -0700

    rerun

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -1,3 +1,9 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
 const product = nums => {
   let results = [];
   let left = 1;
@@ -17,3 +23,4 @@ const product = nums => {
 }
 
 product([1, 2, 3, 4]);
+
