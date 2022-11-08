6b2d3f359cb364523db949c4669f0ae5efff5f8d
nhan <nhanng19@gmail.com>
2022-11-07 18:01:41 -0800

    rerun

--- 3Sum.js
+++ 3Sum.js
@@ -2,27 +2,28 @@
 //
 // Notice that the solution set must not contain duplicate triplets.
 
-sumThree([-1, 0, 1, 2, -1, -4]);
+threeSum([-1, 0, 1, 2, -1, -4]);
 
-const sumThree = nums => {
+const threeSum = nums => {
   let results = [];
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 2; i++) {
     if (nums[i] !== nums[i - 1]) {
       let left = i + 1;
-      let right = nums.length - 1
+      let right = nums.length - 1;
       while (left < right) {
-        let sum = nums[i] + nums[left] + nums[right];
-        if (sum === 0) {
-          results.push([nums[i], nums[left], nums[right]])
+        let currentSum = nums[i] + nums[left] + nums[right];
+        
+        if (currentSum === 0) {
+          results.push([nums[i], nums[left], nums[right]]);
           while (nums[left] === nums[left + 1]) left++;
           while (nums[right] === nums[right - 1]) right--;
           left++;
-          right--;
-        } else if (sum < 0) {
-          left++
-        } else if (sum > 0) {
+          right--
+        } else if (currentSum < 0) {
+          left++;
+        } else if (currentSum > 0) {
           right--;
         }
       }
