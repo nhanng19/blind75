14dacb174d9f2d3103d37baa20c7023d4fc1e149
nhan <nhanng19@gmail.com>
2022-11-07 18:01:57 -0800

    rerun

--- maxProductSubarray.js
+++ maxProductSubarray.js
@@ -6,24 +6,17 @@
 
 maxProductSubArray([2, 3, -2, 4]);
 
+
 const maxProductSubArray = nums => {
   let currentMax = nums[0];
   let currentMin = nums[0];
   let finalMax = nums[0];
 
   for (let i = 0; i < nums.length; i++) {
-    let temp = currentMax
+    let temp = currentMax;
     currentMax = Math.max(Math.max(currentMax * nums[i], currentMin * nums[i]), nums[i]);
     currentMin = Math.max(Math.max(temp * nums[i], currentMin * nums[i]), nums[i]);
     finalMax = Math.max(currentMax, finalMax);
   }
   return finalMax;
-}
-
-const maxProductSubArray = nums => {
-  let currentMax = nums[0];
-  let currentMin = nums[0];
-  let finalMax = nums[0];
-
-  
 }
