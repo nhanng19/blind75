b692e61873e6379a382d67bb0971f40bb6cee049
nhan <nhanng19@gmail.com>
2022-11-14 01:47:07 -0800

    rerun

--- searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray.js
@@ -33,3 +33,29 @@ const arraySearch = (nums, target) => {
   } 
   return -1;
 }
+
+const searchArray = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid
+    }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
