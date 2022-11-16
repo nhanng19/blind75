1785678b1d51cec0c2837391448f29970eed9812
nhan <nhanng19@gmail.com>
2022-11-16 03:17:18 -0800

    rerun

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,17 +1,19 @@
-const container = nums => {
-    let left = 0;
-    let right = nums.length - 1;
-    let result = 0
-    while (left < right) {
-        let smallest = Math.min(nums[left], nums[right]);
-        let area = smallest * (left - right);
-        result = Math.max(result, area);
+const container = (nums) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let result = 0;
+  while (left < right) {
+    let smallest = Math.min(nums[left], nums[right]);
+    let area = smallest * (left - right);
+    result = Math.max(result, area);
 
-        if (nums[left] < nums[right]) {
-            left++;
-        } else {
-            right--;
-        }
+    if (nums[left] < nums[right]) {
+      left++;
+    } else {
+      right--;
     }
-    return result;
-}
+  }
+  return result;
+};
+
+
