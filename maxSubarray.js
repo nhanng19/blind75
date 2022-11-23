d3f3d56bdde9262c7b50e85923d987b4c06d168c
nhan <nhanng19@gmail.com>
2022-11-23 02:40:23 -0800

    rerun

--- maxSubarray.js
+++ maxSubarray.js
@@ -18,16 +18,3 @@ const maxSub = nums => {
   return maxSum;
 }
 
-const subMax = nums => {
-  let sum = 0;
-  let maxSum = nums[0];
-
-  for (let i = 0; i < nums.length; i){
-    if (sum < 0) {
-      sum = 0
-    }
-    sum += nums[i];
-    maxSum = Math.max(maxSum, sum) 
-  }
-  return maxSum
-}
